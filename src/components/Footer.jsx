f78211ca270635cfc9d117ddbc3e9dead9f2c944
7Davronbek <maxmudovdavronbek8@gmail.com>
2022-08-22 15:15:49 +0500

    o4me-app

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,14 +3,14 @@ import React from 'react'
 const Footer = () => {
   return (
     <div className='Footer'>
-        <div className="text-center">
-           <div className="logo">
-            <a href="/">
+      <div className="text-center">
+        <div className="logo">
+          <a href="/">
             <img src="img/footer.png" alt="" />
-            </a>
-           </div>
-                {/* <a target='blank' href="https://muhammadumar.uz/">Сделано с любовью ♥</a> */}
+          </a>
         </div>
+        {/* <a target='blank' href="https://muhammadumar.uz/">Сделано с любовью ♥</a> */}
+      </div>
     </div>
   )
 }
