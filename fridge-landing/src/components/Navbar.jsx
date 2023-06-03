ea2ca97437af0a58eecf43156a50b967c20abca5
7Davronbek <maxmudovdavronbek8@gmail.com>
2023-06-03 16:24:43 +0300

    install dependency

---
+++ fridge-landing/src/components/Navbar.jsx
@@ -0,0 +1,13 @@
+const Navbar = () => {
+  return (
+    <div className="Navbar">
+      <div className="container">
+        <div className="row">
+          <div className="col-12">Navbar</div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
