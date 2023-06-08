5d292137afbb21cb27aae2c42f00bcd3f70d10eb
7Davronbek <maxmudovdavronbek8@gmail.com>
2023-06-09 00:39:01 +0300

    locales

--- fridge-landing/src/locales/UZ.js
+++ fridge-landing/src/locales/UZ.js
@@ -1,12 +1,43 @@
 export const uz = {
-    "navbar1": "ASOSIY",
-    "navbar2": "XIZMATLAR",
-    "navbar3": "BIZ HAQIMIZDA",
-    "navbar4": "JAMOA",
-    "navbar5": "ALOQA",
-    "navbar6": "SHARHLAR",
-}
+  navbar1: "ASOSIY",
+  navbar2: "XIZMATLAR",
+  navbar3: "BIZ HAQIMIZDA",
+  navbar4: "JAMOA",
+  navbar5: "ALOQA",
+  navbar6: "SHARHLAR",
 
+  header1: "Toshkentda muzlatkichlarni ta'mirlash",
+  header2: "Buyurtma qilish",
 
+  services1: "Bizning xizmatlar",
+  services2: "Barcha texnika bilan ishlaymiz",
+  services3: "Maishiy texnikalarni ta'mirlash",
+  services4: "Kir yuvish mashinalarini ta'mirlash",
+  services5: "Muzlatish kameralarini ta'mirlash",
+  services6: "Sovutgichli vitrinlarni ta'mirlash",
+  services7: "Motor ta'mirlash",
+  services8: "Kafolat 6 oydan 4 yilgacha",
+  services9: "Buyurtma berish",
 
+  q1: "Sizning o'z texnikangizni topa olmadingizmi? Hoziroq qo'ng'iroq qiling yoki so'rov qoldiring va operatorimiz sizga kompaniyamizning barcha xizmatlari bo'yicha maslahat beradi!",
+  q2: "Savol berish",
 
+  road1: "Bizning yo'l",
+  road2:
+    "Biz kompaniyani 2013 yilda ochganmiz. Men kompaniya rahbari Vladimir Bondarevman. Men 2000 yildan beri maishiy texnika ta'mirlash sohasida ishlayman. Men asosan muzlatgichlar, muzlatgichlar vitrinlarini ta'mirlash bilan shug'ullanaman. Shu vaqt oralig'ida 1000dan ortiq mijozlarga o'z xizmatimizni ko'rsatib, xalq orasida o'z o'rnimizga ega bo'ldik. Va biz hozirgi kunda ham odamlar mushkulini oson qilish yo'lida, ularga eng yaxshi xizmatlarni ko'rsatib kelmoqdamiz.",
+
+  team1: "Kompaniyamiz mutaxassislari",
+
+  b1: "Biz uchta afzallik bilan ajralib turamiz",
+
+  b2: "Sifatimiz kafolati",
+  b3: "Bajargan ishimiz uchun albatta kafolat beramiz. Chunki barcha mutaxassislarimiz o'z ishining ustalari va ular qilgan ishlari uchun kafolat beradilar.",
+  b4: "1 soatda ta'mirlab berish",
+  b5: "Texnikangiz turi va ta'mirlash ishidan kelib chiqib, 1soat ichida ta'mirlab berishga kafolat beramiz!",
+  b6: "Sifatimiz kafolati",
+  b7: "Bajargan ishimiz uchun albatta kafolat beramiz. Chunki barcha mutaxassislarimiz o'z ishining ustalari va ular qilgan ishlari uchun kafolat beradilar.",
+
+  r1: "Reyting",
+  r2: "11 ta sharh va 11 ta reyting asosida",
+  r3: "Mijozlar fikri",
+};
