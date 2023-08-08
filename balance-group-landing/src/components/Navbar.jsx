26c6b732a67417864479e2389ebf148e9d4b87f1
7Davronbek <maxmudovdavronbek8@gmail.com>
2023-08-08 20:28:25 +0300

    balance group landing

---
+++ balance-group-landing/src/components/Navbar.jsx
@@ -0,0 +1,18 @@
+const Navbar = () => {
+  return (
+    <div className="Navbar">
+      <div className="container">
+        <div className="row">
+          <div className="col-12">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. In omnis
+            molestias laboriosam numquam, ad magnam unde minus magni! Culpa
+            velit distinctio tenetur expedita quia, necessitatibus perspiciatis
+            maxime pariatur maiores mollitia?
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
---
+++ balance-group-landing/src/pages/Main.jsx
@@ -0,0 +1,11 @@
+import { Navbar } from "../components";
+
+const Main = () => {
+  return (
+    <>
+      <Navbar />
+    </>
+  );
+};
+
+export default Main;
