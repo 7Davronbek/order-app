1728d41a578957c5d73b6659903ed548c10783e8
7Davronbek <maxmudovdavronbek8@gmail.com>
2023-10-21 12:56:39 +0500

    sanart landing

--- sanart-landing/js/app.js
+++ sanart-landing/js/app.js
@@ -9,167 +9,169 @@ if (ScrollTrigger.isTouch !== 1) {
   });
 
   gsap.fromTo(
-    ".hero_section",
-    { opacity: 1 },
+    ".wrap",
+    { background: "transparent" },
     {
-      opacity: 0,
+      background: "blue",
+      color: "white",
       scrollTrigger: {
-        end: "500",
+        end: "800",
         scrub: true,
-        start: "center",
-        trigger: ".hero_section",
+        trigger: ".wrap",
       },
     }
   );
-
-  let itemsL = gsap.utils.toArray(".left");
-
-  itemsL.forEach((item) => {
-    gsap.fromTo(
-      item,
-      { opacity: 0.3, x: -100 },
-      {
-        opacity: 1,
-        x: 0,
-        scrollTrigger: {
-          trigger: item,
-          scrub: true,
-        },
-      }
-    );
-  });
-
-  let itemsR = gsap.utils.toArray(".right");
-
-  itemsR.forEach((item) => {
-    gsap.fromTo(
-      item,
-      { opacity: 0.3, y: 100 },
-      {
-        opacity: 1,
-        y: 0,
-        scrollTrigger: {
-          trigger: item,
-          scrub: true,
-        },
-      }
-    );
-  });
 
   gsap.fromTo(
-    ".mySecondDiv",
-    { x: 100 },
+    "#headerShape",
+    { y: 0, x: 0, opacity: 1 },
     {
+      y: 100,
       x: -100,
+      opacity: 0,
       scrollTrigger: {
-        trigger: ".mySecondDiv",
+        trigger: "#headerCol",
         scrub: true,
+        start: "top",
       },
     }
   );
+
+  gsap.fromTo(
+    "#headerBtn",
+    { y: 0, x: 0, opacity: 1 },
+    {
+      y: 90,
+      x: 150,
+      opacity: 0,
+      scrollTrigger: {
+        trigger: "#headerCol",
+        scrub: true,
+        start: "top",
+      },
+    }
+  );
+
+  gsap.fromTo(
+    "#headerP",
+    { y: 0, x: 0, opacity: 1 },
+    {
+      y: 50,
+      x: 100,
+      opacity: 0,
+      scrollTrigger: {
+        trigger: "#headerCol",
+        scrub: true,
+        start: "top",
+      },
+    }
+  );
+
+  gsap.fromTo(
+    "#headerBottom",
+    { y: 0, x: 0, opacity: 1 },
+    {
+      y: 20,
+      x: 0,
+      opacity: 0,
+      scrollTrigger: {
+        trigger: "#headerCol",
+        scrub: true,
+        start: "center",
+      },
+    }
+  );
+
+  gsap.fromTo(
+    "#headerH1Left",
+    { y: 0, x: 0, opacity: 1 },
+    {
+      y: 0,
+      x: 100,
+      opacity: 1,
+      scrollTrigger: {
+        trigger: "#headerBottom",
+        scrub: true,
+        end: "top",
+      },
+    }
+  );
+
+  gsap.fromTo(
+    "#headerH1Right",
+    { y: 0, x: 0, opacity: 1 },
+    {
+      y: 0,
+      x: -200,
+      opacity: 1,
+      scrollTrigger: {
+        trigger: "#headerBottom",
+        scrub: true,
+        end: "top",
+      },
+    }
+  );
+
+  // let itemsL = gsap.utils.toArray(".left");
+
+  // itemsL.forEach((item) => {
+  //   gsap.fromTo(
+  //     item,
+  //     { opacity: 0.3, x: -100 },
+  //     {
+  //       opacity: 1,
+  //       x: 0,
+  //       scrollTrigger: {
+  //         trigger: item,
+  //         scrub: true,
+  //       },
+  //     }
+  //   );
+  // });
+
+  // let itemsR = gsap.utils.toArray(".right");
+
+  // itemsR.forEach((item) => {
+  //   gsap.fromTo(
+  //     item,
+  //     { opacity: 0.3, y: 100 },
+  //     {
+  //       opacity: 1,
+  //       y: 0,
+  //       scrollTrigger: {
+  //         trigger: item,
+  //         scrub: true,
+  //       },
+  //     }
+  //   );
+  // });
+
+  // gsap.fromTo(
+  //   ".mySecondDiv",
+  //   { x: 100 },
+  //   {
+  //     x: -100,
+  //     scrollTrigger: {
+  //       trigger: ".mySecondDiv",
+  //       scrub: true,
+  //     },
+  //   }
+  // );
 }
 
-gsap.fromTo(
-  ".wrap",
-  { background: "transparent" },
-  {
-    background: "blue",
-    color: "white",
-    scrollTrigger: {
-      end: "800",
-      scrub: true,
-      trigger: ".wrap",
-    },
-  }
-);
+function displayCurrentTime() {
+  var currentTime = new Date();
+  var hours = currentTime.getHours();
+  var minutes = currentTime.getMinutes();
+  var seconds = currentTime.getSeconds();
 
-gsap.fromTo(
-  "#headerShape",
-  { y: 0, x: 0, opacity: 1 },
-  {
-    y: 100,
-    x: -100,
-    opacity: 0,
-    scrollTrigger: {
-      trigger: "#headerCol",
-      scrub: true,
-      start: "top",
-    },
-  }
-);
+  hours = (hours < 10 ? "0" : "") + hours;
+  minutes = (minutes < 10 ? "0" : "") + minutes;
+  seconds = (seconds < 10 ? "0" : "") + seconds;
 
+  var formattedTime = hours + ":" + minutes + ":" + seconds;
 
-gsap.fromTo(
-  "#headerBtn",
-  { y: 0, x: 0, opacity: 1 },
-  {
-    y: 90,
-    x: 150,
-    opacity: 0,
-    scrollTrigger: {
-      trigger: "#headerCol",
-      scrub: true,
-      start: "top",
-    },
-  }
-);
+  document.getElementById("current_time").innerHTML = formattedTime;
+}
 
-gsap.fromTo(
-  "#headerP",
-  { y: 0, x: 0, opacity: 1 },
-  {
-    y: 50,
-    x: 100,
-    opacity: 0,
-    scrollTrigger: {
-      trigger: "#headerCol",
-      scrub: true,
-      start: "top",
-    },
-  }
-);
-
-gsap.fromTo(
-  "#headerBottom",
-  { y: 0, x: 0, opacity: 1 },
-  {
-    y: 20,
-    x: 0,
-    opacity: 0,
-    scrollTrigger: {
-      trigger: "#headerCol",
-      scrub: true,
-      start: "center",
-    },
-  }
-);
-
-gsap.fromTo(
-  "#headerH1Left",
-  { y: 0, x: 0, opacity: 1 },
-  {
-    y: 0,
-    x: 100,
-    opacity: 1,
-    scrollTrigger: {
-      trigger: "#headerBottom",
-      scrub: true,
-      end: "top",
-    },
-  }
-);
-
-gsap.fromTo(
-  "#headerH1Right",
-  { y: 0, x: 0, opacity: 1 },
-  {
-    y: 0,
-    x: -200,
-    opacity: 1,
-    scrollTrigger: {
-      trigger: "#headerBottom",
-      scrub: true,
-      end: "top",
-    },
-  }
-);
+setInterval(displayCurrentTime, 1000);
