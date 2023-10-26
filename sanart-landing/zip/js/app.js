596d0b2663e020bb8eff54cdf459a265728a7143
7Davronbek <maxmudovdavronbek8@gmail.com>
2023-10-26 08:52:28 +0500

    sanart landing

--- sanart-landing/zip/js/app.js
+++ sanart-landing/zip/js/app.js
@@ -1,12 +1,12 @@
 gsap.registerPlugin(ScrollTrigger, ScrollSmoother);
 
 if (ScrollTrigger.isTouch !== 1) {
-  // ScrollSmoother.create({
-  //   wrapper: ".wrapper",
-  //   content: ".content",
-  //   smooth: 1.5,
-  //   effects: true,
-  // });
+  ScrollSmoother.create({
+    wrapper: ".wrapper",
+    content: ".content",
+    smooth: 1.5,
+    effects: true,
+  });
 
   gsap.fromTo(
     ".wrap",
