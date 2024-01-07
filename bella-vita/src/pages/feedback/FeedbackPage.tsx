c8e471f5bc005ae9d7ecd46cf92c691a1ba2ad2a
7Davronbek <maxmudovdavronbek8@gmail.com>
2024-01-07 21:12:04 +0500

    bella vita

--- bella-vita/src/pages/feedback/FeedbackPage.tsx
+++ bella-vita/src/pages/feedback/FeedbackPage.tsx
@@ -1,24 +1,40 @@
-import {Feedback} from "../../components";
+import {Feedback, Map} from "../../components";
+import circle from "@/assets/circle.png"
 import feedbackBg2 from "@/assets/feedbackBg2.png"
 
 const FeedbackPage = () => {
     return (
-        <div className='FeedbackPage miniFeedback'>
-            <div className="container">
-                <div className="row">
-                    <div className="col-lg-10">
-                        <Feedback
-                            type={"Feedback"}
-                            mainText={"Biz bilan bog’laning"}
-                            secondaryText={"Aloqa"}
-                        />
-                    </div>
-                    <div className="col-lg-2">
-                        <img src={feedbackBg2} alt=""/>
+        <>
+            <div className='FeedbackPage miniFeedback'>
+                <div className="container">
+                    <div className="row">
+                        <div className="col-lg-10">
+                            <Feedback
+                                type={"Feedback"}
+                                mainText={"Biz bilan bog’laning"}
+                                secondaryText={"Aloqa"}
+                            />
+                        </div>
+                        <div className="col-lg-2">
+                            <img src={feedbackBg2} alt=""/>
+                        </div>
                     </div>
                 </div>
             </div>
-        </div>
+            <div className="PhoneNumber mb-5">
+                <div className="container">
+                    <div className="row">
+                        <div className="col-12">
+                            <div className='secondaryText'><img className='circle' src={circle}
+                                                                alt=""/> Telefon raqam
+                            </div>
+                            <a href={"tel:"} className="mainText">+998 90 009 09 09</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <Map/>
+        </>
     );
 };
 
