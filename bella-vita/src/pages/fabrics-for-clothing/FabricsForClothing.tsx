31353c1007f70676cd607bace65425f2aeb21883
7Davronbek <maxmudovdavronbek8@gmail.com>
2024-01-07 07:43:34 +0500

    bella vita

--- bella-vita/src/pages/fabrics-for-clothing/FabricsForClothing.tsx
+++ bella-vita/src/pages/fabrics-for-clothing/FabricsForClothing.tsx
@@ -2,6 +2,12 @@ import circle from "@/assets/circle.png"
 import tool from "@/assets/tool.png"
 import {Feedback} from "@/components";
 
+import {Swiper, SwiperSlide} from 'swiper/react';
+import {Navigation} from 'swiper/modules';
+
+import 'swiper/css';
+import 'swiper/css/navigation';
+
 const FabricsForClothing = () => {
     return (
         <>
@@ -9,16 +15,224 @@ const FabricsForClothing = () => {
                 <div className="container">
                     <div className="row">
                         <div className="col-12">
-                            <h1 className='secondaryText text-center mx-auto'><img className='circle' src={circle}
-                                                                                   alt=""/> Matolar
+                            <h1 className='secondaryText text-center '>
+                                <img className='circle center' src={circle}
+                                     alt=""/> Matolar
                             </h1>
-                            <h2 className='mainText'>Liboslar uchun matolar</h2>
+                            <h2 className='mainText text-center'>Liboslar uchun matolar</h2>
+                        </div>
+                    </div>
+                    <div className="row mt-5">
+                        <div className="col-lg-4">
+
+                            <div className="cards">
+
+                                <Swiper
+                                    navigation={true} loop={true} modules={[Navigation]}
+                                    className="mySwiper"
+                                >
+                                    <SwiperSlide>
+                                        <div className="images">
+                                            <img src={tool} className='w-100' alt=""/>
+                                        </div>
+                                    </SwiperSlide>
+                                    <SwiperSlide>
+                                        <div className="images">
+                                            <img src={tool} className='w-100' alt=""/>
+                                        </div>
+                                    </SwiperSlide>
+                                    <SwiperSlide>
+                                        <div className="images">
+                                            <img src={tool} className='w-100' alt=""/>
+                                        </div>
+                                    </SwiperSlide>
+                                </Swiper>
+                                <h1>Atlas</h1>
+                                <h2>Shaxsiy tikuvchilik - bu ko'plab muhim bosqichlardan iborat bo'lgan to'laqonli
+                                    tasvir ustidagi mashaqqatli ish bo'lib, ulardan biri mos materiallarni tanlash bilan
+                                    bog'liq.</h2>
+                                <h3>20 000 000 сум</h3>
+                            </div>
                         </div>
                         <div className="col-lg-4">
+
                             <div className="cards">
-                                <div className="images">
-                                    <img src={tool} className='w-100' alt=""/>
-                                </div>
+
+                                <Swiper
+                                    navigation={true} loop={true} modules={[Navigation]}
+                                    className="mySwiper"
+                                >
+                                    <SwiperSlide>
+                                        <div className="images">
+                                            <img src={tool} className='w-100' alt=""/>
+                                        </div>
+                                    </SwiperSlide>
+                                    <SwiperSlide>
+                                        <div className="images">
+                                            <img src={tool} className='w-100' alt=""/>
+                                        </div>
+                                    </SwiperSlide>
+                                    <SwiperSlide>
+                                        <div className="images">
+                                            <img src={tool} className='w-100' alt=""/>
+                                        </div>
+                                    </SwiperSlide>
+                                </Swiper>
+                                <h1>Atlas</h1>
+                                <h2>Shaxsiy tikuvchilik - bu ko'plab muhim bosqichlardan iborat bo'lgan to'laqonli
+                                    tasvir ustidagi mashaqqatli ish bo'lib, ulardan biri mos materiallarni tanlash bilan
+                                    bog'liq.</h2>
+                                <h3>20 000 000 сум</h3>
+                            </div>
+                        </div>
+                        <div className="col-lg-4">
+
+                            <div className="cards">
+
+                                <Swiper
+                                    navigation={true} loop={true} modules={[Navigation]}
+                                    className="mySwiper"
+                                >
+                                    <SwiperSlide>
+                                        <div className="images">
+                                            <img src={tool} className='w-100' alt=""/>
+                                        </div>
+                                    </SwiperSlide>
+                                    <SwiperSlide>
+                                        <div className="images">
+                                            <img src={tool} className='w-100' alt=""/>
+                                        </div>
+                                    </SwiperSlide>
+                                    <SwiperSlide>
+                                        <div className="images">
+                                            <img src={tool} className='w-100' alt=""/>
+                                        </div>
+                                    </SwiperSlide>
+                                </Swiper>
+                                <h1>Atlas</h1>
+                                <h2>Shaxsiy tikuvchilik - bu ko'plab muhim bosqichlardan iborat bo'lgan to'laqonli
+                                    tasvir ustidagi mashaqqatli ish bo'lib, ulardan biri mos materiallarni tanlash bilan
+                                    bog'liq.</h2>
+                                <h3>20 000 000 сум</h3>
+                            </div>
+                        </div>
+                        <div className="col-lg-4">
+
+                            <div className="cards">
+
+                                <Swiper
+                                    navigation={true} loop={true} modules={[Navigation]}
+                                    className="mySwiper"
+                                >
+                                    <SwiperSlide>
+                                        <div className="images">
+                                            <img src={tool} className='w-100' alt=""/>
+                                        </div>
+                                    </SwiperSlide>
+                                    <SwiperSlide>
+                                        <div className="images">
+                                            <img src={tool} className='w-100' alt=""/>
+                                        </div>
+                                    </SwiperSlide>
+                                    <SwiperSlide>
+                                        <div className="images">
+                                            <img src={tool} className='w-100' alt=""/>
+                                        </div>
+                                    </SwiperSlide>
+                                </Swiper>
+                                <h1>Atlas</h1>
+                                <h2>Shaxsiy tikuvchilik - bu ko'plab muhim bosqichlardan iborat bo'lgan to'laqonli
+                                    tasvir ustidagi mashaqqatli ish bo'lib, ulardan biri mos materiallarni tanlash bilan
+                                    bog'liq.</h2>
+                                <h3>20 000 000 сум</h3>
+                            </div>
+                        </div>
+                        <div className="col-lg-4">
+
+                            <div className="cards">
+
+                                <Swiper
+                                    navigation={true} loop={true} modules={[Navigation]}
+                                    className="mySwiper"
+                                >
+                                    <SwiperSlide>
+                                        <div className="images">
+                                            <img src={tool} className='w-100' alt=""/>
+                                        </div>
+                                    </SwiperSlide>
+                                    <SwiperSlide>
+                                        <div className="images">
+                                            <img src={tool} className='w-100' alt=""/>
+                                        </div>
+                                    </SwiperSlide>
+                                    <SwiperSlide>
+                                        <div className="images">
+                                            <img src={tool} className='w-100' alt=""/>
+                                        </div>
+                                    </SwiperSlide>
+                                </Swiper>
+                                <h1>Atlas</h1>
+                                <h2>Shaxsiy tikuvchilik - bu ko'plab muhim bosqichlardan iborat bo'lgan to'laqonli
+                                    tasvir ustidagi mashaqqatli ish bo'lib, ulardan biri mos materiallarni tanlash bilan
+                                    bog'liq.</h2>
+                                <h3>20 000 000 сум</h3>
+                            </div>
+                        </div>
+                        <div className="col-lg-4">
+
+                            <div className="cards">
+
+                                <Swiper
+                                    navigation={true} loop={true} modules={[Navigation]}
+                                    className="mySwiper"
+                                >
+                                    <SwiperSlide>
+                                        <div className="images">
+                                            <img src={tool} className='w-100' alt=""/>
+                                        </div>
+                                    </SwiperSlide>
+                                    <SwiperSlide>
+                                        <div className="images">
+                                            <img src={tool} className='w-100' alt=""/>
+                                        </div>
+                                    </SwiperSlide>
+                                    <SwiperSlide>
+                                        <div className="images">
+                                            <img src={tool} className='w-100' alt=""/>
+                                        </div>
+                                    </SwiperSlide>
+                                </Swiper>
+                                <h1>Atlas</h1>
+                                <h2>Shaxsiy tikuvchilik - bu ko'plab muhim bosqichlardan iborat bo'lgan to'laqonli
+                                    tasvir ustidagi mashaqqatli ish bo'lib, ulardan biri mos materiallarni tanlash bilan
+                                    bog'liq.</h2>
+                                <h3>20 000 000 сум</h3>
+                            </div>
+                        </div>
+                        <div className="col-lg-4">
+
+                            <div className="cards">
+
+                                <Swiper
+                                    navigation={true} loop={true} modules={[Navigation]}
+                                    className="mySwiper"
+                                >
+                                    <SwiperSlide>
+                                        <div className="images">
+                                            <img src={tool} className='w-100' alt=""/>
+                                        </div>
+                                    </SwiperSlide>
+                                    <SwiperSlide>
+                                        <div className="images">
+                                            <img src={tool} className='w-100' alt=""/>
+                                        </div>
+                                    </SwiperSlide>
+                                    <SwiperSlide>
+                                        <div className="images">
+                                            <img src={tool} className='w-100' alt=""/>
+                                        </div>
+                                    </SwiperSlide>
+                                </Swiper>
                                 <h1>Atlas</h1>
                                 <h2>Shaxsiy tikuvchilik - bu ko'plab muhim bosqichlardan iborat bo'lgan to'laqonli
                                     tasvir ustidagi mashaqqatli ish bo'lib, ulardan biri mos materiallarni tanlash bilan
--- bella-vita/src/pages/vacancy/Vacancy.tsx
+++ bella-vita/src/pages/vacancy/Vacancy.tsx
@@ -1,9 +1,112 @@
+import circle from "@/assets/circle.png"
+import vacancy1 from "@/assets/vacancy1.png"
+import vacancy2 from "@/assets/vacancy2.png"
 
 const Vacancy = () => {
     return (
-        <>
-            Vacancy
-        </>
+        <div className='Vacancy'>
+            <div className="container">
+                <div className="row">
+                    <div className="col-12">
+                        <h1 className='secondaryText text-center '>
+                            <img className='circle center' src={circle}
+                                 alt=""/> Vakansiya
+                        </h1>
+                        <h2 className='mainText text-center'>O’zingizga mos ish o’rnini toping</h2>
+                    </div>
+                </div>
+                <div className="row mt-5">
+                    <div className="col-lg-6">
+                        <div className="cards">
+                            <div className="image">
+                                <img src={vacancy1} alt=""/>
+                            </div>
+                            <div className="wrap">
+                                <div className="flex">
+                                    <h3>ISH turi:</h3>
+                                    <h4>Kassir</h4>
+                                </div>
+                                <div className="flex">
+                                    <h3>Ish vaqti:</h3>
+                                    <h4 className={'end'}>Dushanbadan-shanbagacha 9:00-18:00</h4>
+                                </div>
+                                <div className="flex">
+                                    <h3>Oylik:</h3>
+                                    <h4 className='bold'>5 000 000 so’m</h4>
+                                </div>
+                                <a href="tel:">Bizga bog’laning</a>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="col-lg-6">
+                        <div className="cards">
+                            <div className="image">
+                                <img src={vacancy2} alt=""/>
+                            </div>
+                            <div className="wrap">
+                                <div className="flex">
+                                    <h3>ISH turi:</h3>
+                                    <h4>Kassir</h4>
+                                </div>
+                                <div className="flex">
+                                    <h3>Ish vaqti:</h3>
+                                    <h4 className={'end'}>Dushanbadan-shanbagacha 9:00-18:00</h4>
+                                </div>
+                                <div className="flex">
+                                    <h3>Oylik:</h3>
+                                    <h4 className='bold'>5 000 000 so’m</h4>
+                                </div>
+                                <a href="tel:">Bizga bog’laning</a>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="col-lg-6">
+                        <div className="cards">
+                            <div className="image">
+                                <img src={vacancy2} alt=""/>
+                            </div>
+                            <div className="wrap">
+                                <div className="flex">
+                                    <h3>ISH turi:</h3>
+                                    <h4>Kassir</h4>
+                                </div>
+                                <div className="flex">
+                                    <h3>Ish vaqti:</h3>
+                                    <h4 className={'end'}>Dushanbadan-shanbagacha 9:00-18:00</h4>
+                                </div>
+                                <div className="flex">
+                                    <h3>Oylik:</h3>
+                                    <h4 className='bold'>5 000 000 so’m</h4>
+                                </div>
+                                <a href="tel:">Bizga bog’laning</a>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="col-lg-6">
+                        <div className="cards">
+                            <div className="image">
+                                <img src={vacancy1} alt=""/>
+                            </div>
+                            <div className="wrap">
+                                <div className="flex">
+                                    <h3>ISH turi:</h3>
+                                    <h4>Kassir</h4>
+                                </div>
+                                <div className="flex">
+                                    <h3>Ish vaqti:</h3>
+                                    <h4 className={'end'}>Dushanbadan-shanbagacha 9:00-18:00</h4>
+                                </div>
+                                <div className="flex">
+                                    <h3>Oylik:</h3>
+                                    <h4 className='bold'>5 000 000 so’m</h4>
+                                </div>
+                                <a href="tel:">Bizga bog’laning</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
     );
 };
 
